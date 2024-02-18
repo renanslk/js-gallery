1c773369e986173986036ec64e3ab67e8f1f2b9e
Renan Pelogia <pelogia2@hotmail.com>
2024-02-18 13:39:07 -0300

    Added first gallery sources

---
+++ gallery/data.js
@@ -0,0 +1,11 @@
+let data = [
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\001.png",["red","stripes","line"], "Lilly"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\002.png",["green", "squares"], "Lilly"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\003.png",["yellow","circles"], "Bob"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\004.png",["blue", "line"], "Bob"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\005.png",["pink"], "John"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\006.png",["blue", "squares"], "Lilly"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\007.png",["gray","squares","circles"], "John"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\008.png",["blue", "green", "squares"], "Clair"],
+    ["C:\\Users\\Renan\\Documents\\Javascript\\images\\009.png",["green", "gray", "blue", "red"], "Clair"],
+];
